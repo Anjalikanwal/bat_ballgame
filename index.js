5c0f4fcd16320e973bb3de55d7b1b0b4a211efc8
Anjali kumari <195102237+Anjalikanwal@users.noreply.github.com>
2026-07-06 21:02:03 +0000

    add data in local storage

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-let scores={
+let local_string= localStorage.getItem('scores'); 
+let scores;
+resetScores(local_string);
+
+
+function resetScores(local_string){
+scores= local_string ? JSON.parse(local_string):{
     win:0,
     lost:0,
     tie:0,
-    display_score:function(){return  `win:${scores.win} , lost:${scores.lost} ,tie :${scores.tie}`}
-
+   
 };
+scores.display_score =function(){return  `win:${scores.win} , lost:${scores.lost} ,tie :${scores.tie}`};
+display();
+}
 function generate_comp_choice(){
             let RandomNum= Math.random()*3;
             if(RandomNum<=1){
@@ -75,11 +83,14 @@ function comparison (usermove,CompMove){
 }
 
 function display(userMove , CompMove, result){
-    console.log(alert(`user Move is ${userMove} and compMove is ${CompMove} then  ${result}
-        ${scores.display_score()}`))
+    localStorage.setItem('scores' , JSON.stringify(scores));
+    document.querySelector("#user_move"). innerText= userMove !==undefined? `you have chosen ${userMove}` :' ';
+    document.querySelector("#computer_move"). innerText=CompMove !==undefined?`computer have chosen ${CompMove}`: ' ';
+    document.querySelector('#result') .innerText= result || ' ';
+    document.querySelector('#score').innerText= scores.display_score();
+    
 }
 
 
 
 
-console.log("hello");
