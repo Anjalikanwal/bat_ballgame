ff7ce84aff0937df2e592003fc96a23faf1b8e3f
Anjali kumari <195102237+Anjalikanwal@users.noreply.github.com>
2026-07-06 17:43:43 +0000

    add objects

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let scores={
+    win:0,
+    lost:0,
+    tie:0,
+    display_score:function(){return  `win:${scores.win} , lost:${scores.lost} ,tie :${scores.tie}`}
+
+};
 function generate_comp_choice(){
             let RandomNum= Math.random()*3;
             if(RandomNum<=1){
@@ -14,40 +21,65 @@ function comparison (usermove,CompMove){
     if ( usermove =='bat' ){
         
             if(CompMove==='bat'){
+                scores.tie++;
                 return 'its a tie';
+                
             }
             else if(CompMove==='ball'){
+                scores.win++;
                 return'user won';
+                
             } 
             else{
+                scores.lost++;
                 return 'comp won';
+                
             }
     }
     else if(usermove=='ball'){
          if(CompMove === 'bat'){
+         scores.win++;
         return 'user won';
+        
         }
        else if(CompMove === 'ball'){
+        scores.tie++;
         return  'its a tie';
+        
        } 
       else{
+        scores.lost++;
         return 'computer won';
+        
     }
    }
    else if(usermove=='stump'){
             if(CompMove==='bat'){
+                scores.lost++;
                 return 'computer won';
+                
+
             }
             else if(CompMove==='ball'){
+                scores.win++;
                 return 'user won';
+                
             } 
             else{
+                scores.tie++;
                 return'its a tie';
+                
             }
    }
 
 }
 
 function display(userMove , CompMove, result){
-    console.log(alert(`user Move is ${userMove} and compMove is ${CompMove} then  ${result}`))
+    console.log(alert(`user Move is ${userMove} and compMove is ${CompMove} then  ${result}
+        ${scores.display_score()}`))
 }
+
+
+
+
+console.log("hello");
